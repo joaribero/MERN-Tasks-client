f645fa65ddd117c67d9f8222e91e7939483fcc58
Joaribero <joaribero_la12@hotmail.com>
2021-08-05 13:15:12 -0300

    Configure the project and adding the routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Projects from './components/projects/Projects.jsx';
+import Login from './components/auth/Login.jsx';
+import NewAccount from './components/auth/NewAccount.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/new-account" component={NewAccount}/>
+        <Route exact path="/projects" component={Projects}/>
+      </Switch>
+    </Router>
   );
 }
 
