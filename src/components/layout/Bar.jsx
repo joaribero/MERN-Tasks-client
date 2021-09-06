9d34c15ec9e8a6ada0e3e16b1207c2024352ae1a
Joaribero <joaribero_la12@hotmail.com>
2021-09-06 20:13:20 -0300

    Show username on bar

--- src/components/layout/Bar.jsx
+++ src/components/layout/Bar.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React,{useContext,useEffect} from 'react';
+import AuthContext from '../../context/auth/authContext';
 
 const Bar = () => {
+
+    //extract auth information
+    const authContext = useContext(AuthContext);
+    const {user, userAuthenticated} = authContext;
+
+    useEffect(() => {
+
+        userAuthenticated();
+
+    },[]);
+
+
+
     return (  
         <header className="app-header">
-            <p className="nombre-usuario">Hi <span>Joa Ribero</span>!</p>
+            {user ? <p className="nombre-usuario">Hi <span>{user.name}</span>!</p> : null}          
             <nav className="nav-principal">
                 <a href="#!">Log Out</a>
             </nav>
